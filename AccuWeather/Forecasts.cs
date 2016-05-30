18aff2f88a4e3be793a0fcc36e8d27d4ebd33744
demonic-bling <gauthamvinod@outlook.com>
2016-05-30 22:28:55 +0530

    Gets 12 Hour Forecast API and Displays in a Listiew
    Wind Speed And Temperature Grouped Together

---
+++ AccuWeather/Forecasts.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AccuWeather
+{
+    public class Temperature
+    {
+        public double Value { get; set; }
+        public string Unit { get; set; }
+        public int UnitType { get; set; }
+    }
+
+    public class RealFeelTemperature
+    {
+        public double Value { get; set; }
+        public string Unit { get; set; }
+        public int UnitType { get; set; }
+    }
+
+    public class WetBulbTemperature
+    {
+        public double Value { get; set; }
+        public string Unit { get; set; }
+        public int UnitType { get; set; }
+    }
+
+    public class DewPoint
+    {
+        public double Value { get; set; }
+        public string Unit { get; set; }
+        public int UnitType { get; set; }
+    }
+
+    public class Speed
+    {
+        public double Value { get; set; }
+        public string Unit { get; set; }
+        public int UnitType { get; set; }
+    }
+
+    public class Direction
+    {
+        public int Degrees { get; set; }
+        public string Localized { get; set; }
+        public string English { get; set; }
+    }
+
+    public class Wind
+    {
+        public Speed Speed { get; set; }
+        public Direction Direction { get; set; }
+    }
+
+    public class Speed2
+    {
+        public double Value { get; set; }
+        public string Unit { get; set; }
+        public int UnitType { get; set; }
+    }
+
+    public class WindGust
+    {
+        public Speed2 Speed { get; set; }
+    }
+
+    public class Visibility
+    {
+        public double Value { get; set; }
+        public string Unit { get; set; }
+        public int UnitType { get; set; }
+    }
+
+    public class Ceiling
+    {
+        public double Value { get; set; }
+        public string Unit { get; set; }
+        public int UnitType { get; set; }
+    }
+
+    public class TotalLiquid
+    {
+        public double Value { get; set; }
+        public string Unit { get; set; }
+        public int UnitType { get; set; }
+    }
+
+    public class Rain
+    {
+        public double Value { get; set; }
+        public string Unit { get; set; }
+        public int UnitType { get; set; }
+    }
+
+    public class Snow
+    {
+        public double Value { get; set; }
+        public string Unit { get; set; }
+        public int UnitType { get; set; }
+    }
+
+    public class Ice
+    {
+        public double Value { get; set; }
+        public string Unit { get; set; }
+        public int UnitType { get; set; }
+    }
+
+    public class Forecasts
+    {
+        public string DateTime { get; set; }
+        public int EpochDateTime { get; set; }
+        public int WeatherIcon { get; set; }
+        public string IconPhrase { get; set; }
+        public bool IsDaylight { get; set; }
+        public Temperature Temperature { get; set; }
+        public RealFeelTemperature RealFeelTemperature { get; set; }
+        public WetBulbTemperature WetBulbTemperature { get; set; }
+        public DewPoint DewPoint { get; set; }
+        public Wind Wind { get; set; }
+        public WindGust WindGust { get; set; }
+        public int RelativeHumidity { get; set; }
+        public Visibility Visibility { get; set; }
+        public Ceiling Ceiling { get; set; }
+        public int UVIndex { get; set; }
+        public string UVIndexText { get; set; }
+        public int PrecipitationProbability { get; set; }
+        public int RainProbability { get; set; }
+        public int SnowProbability { get; set; }
+        public int IceProbability { get; set; }
+        public TotalLiquid TotalLiquid { get; set; }
+        public Rain Rain { get; set; }
+        public Snow Snow { get; set; }
+        public Ice Ice { get; set; }
+        public int CloudCover { get; set; }
+        public string MobileLink { get; set; }
+        public string Link { get; set; }
+    }
+}
---
+++ AccuWeather/MainPage.xaml.cs
@@ -0,0 +1,192 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Data.Json;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using System.Threading.Tasks;
+using Windows.Devices.Geolocation;
+using Windows.Storage;
+using Windows.UI.Popups;
+using System.Collections.ObjectModel;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=391641
+
+// API key : VGGxNAsElgGyskitNGuw4aSxz2hUcWl5 
+
+namespace AccuWeather
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+
+        public ObservableCollection<Forecasts> Forecast = new ObservableCollection<Forecasts>();
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+
+            this.NavigationCacheMode = NavigationCacheMode.Required;
+        }
+
+        /// <summary>
+        /// Invoked when this page is about to be displayed in a Frame.
+        /// </summary>
+        /// <param name="e">Event data that describes how this page was reached.
+        /// This parameter is typically used to configure the page.</param>
+        protected override async void OnNavigatedTo(NavigationEventArgs e)
+        {
+            // TODO: Prepare page for display here.
+
+            // TODO: If your application contains multiple pages, ensure that you are
+            // handling the hardware Back button by registering for the
+            // Windows.Phone.UI.Input.HardwareButtons.BackPressed event.
+            // If you are using the NavigationHelper provided by some templates,
+            // this event is handled for you.
+
+            var localSettings = Windows.Storage.ApplicationData.Current.LocalSettings;
+            if (localSettings.Values.ContainsKey("LocationConsent"))
+            { 
+                // User has opted in or out of Location
+                
+            }
+            else
+            {
+                var dialog = new MessageDialog("We need your current location.");
+                dialog.Title = "Please";
+                dialog.Commands.Add(new UICommand { Label = "Ok", Id = 0 });
+                dialog.Commands.Add(new UICommand { Label = "Cancel", Id = 1 });
+                var res = await dialog.ShowAsync();
+
+
+                if ((int)res.Id == 0)
+                {
+                   localSettings.Values["LocationConsent"] = true;
+                }
+                else
+                {
+                    localSettings.Values["LocationConsent"] = false;
+                }
+
+            }
+
+            Tuple<string, string> pos = await GetLocAsync();
+
+            string loc_key = await GetLocKeyAsync(pos.Item1, pos.Item2);
+
+            List<Forecasts> obj = new List<Forecasts>();
+            obj = await GetForecastAsync(loc_key);
+
+            loading.Visibility = Windows.UI.Xaml.Visibility.Collapsed;
+
+            foreach(Forecasts f in obj)
+            {
+                if(f.Temperature.Unit == "F" || f.Temperature.Unit == "f" )
+                {
+                    f.Temperature.Unit = "C";
+                    f.Temperature.Value = ((f.Temperature.Value - 32)* 5)/ 9;
+                }
+                string s = f.DateTime;
+                f.DateTime = "Time : " + s.Substring(11,8);
+                Forecast.Add(f);
+            }
+
+            ListV.ItemsSource = Forecast;
+
+       
+    }
+
+        public async Task<Tuple<string, string>> GetLocAsync()
+        {
+
+            var localSettings = Windows.Storage.ApplicationData.Current.LocalSettings;
+            string lat, lon;
+
+            if (localSettings.Values.ContainsKey("LocationConsent") != true)
+            {
+                // User has opted in or out of Location
+                 return Tuple.Create("", "");
+            }
+
+            Geolocator geolocator = new Geolocator();
+            geolocator.DesiredAccuracyInMeters = 50;
+
+            try
+            {
+                Geoposition geoposition = await geolocator.GetGeopositionAsync(
+                    maximumAge: TimeSpan.FromMinutes(5),
+                    timeout: TimeSpan.FromSeconds(10)
+                    );
+
+                lat = geoposition.Coordinate.Point.Position.Latitude.ToString("0.00");
+                 
+                lon = geoposition.Coordinate.Point.Position.Longitude.ToString("0.00");
+
+                return Tuple.Create(lat, lon);
+            }
+            catch (Exception ex)
+            {
+                ex.ToString();
+                return Tuple.Create("", "");
+            }
+        }
+
+        public async Task<string> GetLocKeyAsync(string lat, string lon)
+        {
+            try
+            {
+                using (HttpClient client = new HttpClient())
+                {
+                    var response = await client.GetStringAsync("http://dataservice.accuweather.com/locations/v1/cities/geoposition/search?apikey=VGGxNAsElgGyskitNGuw4aSxz2hUcWl5&q=" + lat + "%2C" + lon);
+
+                    JsonObject obj = JsonObject.Parse(response);
+                    string loc = obj["Key"].GetString();
+
+                    return loc;
+                }
+            }
+            catch (Exception ex)
+            {
+                return ex.ToString();
+            }
+        }
+
+        public async Task<List<Forecasts>> GetForecastAsync(string key)
+        {
+            try
+            {
+                using (HttpClient client = new HttpClient())
+                {
+
+                    var response = await client.GetStringAsync("http://dataservice.accuweather.com/forecasts/v1/hourly/12hour/" + key + "?apikey=VGGxNAsElgGyskitNGuw4aSxz2hUcWl5&details=true");
+                    List<Forecasts> obj = new List<Forecasts>();
+                    obj = JsonConvert.DeserializeObject<List<Forecasts>>(response);
+                    return obj;
+
+                }
+            }
+            catch (Exception ex)
+            {
+                ex.ToString();
+                return null;
+            }
+        }
+
+        
+}
+}
